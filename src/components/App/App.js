e850301b4096bd5ce39cc90d9a06859c75a76b19
Rashevskyyy <keepcalm312000@gmail.com>
2021-07-15 19:41:56 +0300

    fix comments and bugs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,14 @@ import Main from '../Main';
 import Registration from '../Registration'
 import Auth from '../Auth';
 import { Route, Switch } from 'react-router-dom';
-
+import Header from '../Header/Header';
 import { GlobalStyle } from '../../utils/variablesStyle';
 
 const App = () => {
     return (
         <>
-            <Header />
             <GlobalStyle />
+            <Header />
             <Switch>
                 <Route exact path='/' component={Auth} />
                 <Route path='/main' component={Main}/>
--- src/components/Header/HeaderControlPanel/HeaderControlPanel.tsx
+++ src/components/Header/HeaderControlPanel/HeaderControlPanel.tsx
@@ -1,51 +1,33 @@
 import React from 'react';
 import { StControl } from 'src/components/Header/HeaderControlPanel/styled';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { HEADER_CONTROL_BTNS } from 'src/constants/componentsConsts';
-import Button  from '../../UI/Button';
+import ButtonsHeader  from '../../UI/Button/ButtonsHeader';
 import { colorDefault } from '../../UI/baseLayout';
-import { support } from '../../../helpers/support';
 
-const HeaderControlPanel = ({ setValue }) => {
-    const { i18n } = useTranslation();
-    const handleChangeLanguage = (e) => {
-        i18n.changeLanguage(e.target.value);
-        localStorage.setItem('lang', e.target.value);
-    };
 
-    const handleThemeClick = ({ target }) => {
-        support.setSessionStorageItem('themeMode', target.value);
-        setValue({ name: 'themeMode', value: target.value });
-    };
-
-    const getFunctionForButtons = (el) => {
-        switch (el.id) {
-            case 'theme_btn': return handleThemeClick;
-            default: return handleChangeLanguage;
-        }
-    };
-
+const HeaderControlPanel = () => {
     return(
-        <StControl>
-            {HEADER_CONTROL_BTNS.map((el) => {
-                // if (el.value === themeMode) return null;
-                return (
-                    <Button
-                        id={el.id}
-                        content={el.content}
-                        key={el.id}
-                        color={colorDefault}
-                        fontSize='26px'
-                        width='60px'
-                        height="10vh"
-                        borderRadius="0px"
-                        value={el.value}
-                        bgColor="transparent"
-                        onClick={getFunctionForButtons(el)}
-                    />
-                );
-            })}
-        </StControl>
+    <StControl>
+        {HEADER_CONTROL_BTNS.map((el) => {
+            // if (el.value === themeMode) return null;
+            return (
+                <ButtonsHeader
+                    id={el.id}
+                    content={el.content}
+                    key={el.id}
+                    color={colorDefault}
+                    fontSize='26px'
+                    width='60px'
+                    height="10vh"
+                    borderRadius="0px"
+                    value={el.value}
+                    bgColor="transparent"
+                    onClick={onclick}
+                />
+            );
+        })}
+    </StControl>
     )
 }
 
