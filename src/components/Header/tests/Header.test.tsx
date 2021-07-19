3acb571e09a0b903d784b37571f803b01ff77df3
Alexei Rashevskyi <keepcalm312000@gmail.com>
2021-07-19 11:54:23 +0300

    added jest config file and test in Header component

---
+++ src/components/Header/tests/Header.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from '../Header';
+import { shallowSmart, mountSmart } from '/src/helpers/testHelper';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore();
+const store = mockStore({
+    user: {
+        themeMode: 'light',
+    },
+});
+
+describe('Header', () => {
+    it('Should match snapshot', () => {
+        const component = shallowSmart(<Header />, store);
+        expect(component.html()).toMatchSnapshot();
+    });
+    it('should render p', () => {
+        const component = mountSmart(<Header />, store);
+        expect(component.find('Logo')).toHaveLength(1);
+    });
+    it('should render inputs', () => {
+        const component = mountSmart(<Header />, store);
+        expect(component.find('HeaderControlPanel')).toHaveLength(1);
+    });
+});
