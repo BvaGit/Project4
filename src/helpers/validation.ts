a779dce1323b1b769cfa69045b02481678c2830d
Rashevskyyy <keepcalm312000@gmail.com>
2021-07-20 21:37:54 +0300

    fix localization in validation, added ru language

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,16 +1,19 @@
 import validationRegExp from '../constants/validation'
+import i18n from 'i18next';
 
 export const validateRegField = (field: string, value: string) => {
     const {loginValidation, passwordValidation} = validationRegExp
     switch(field){
         case 'login':
             if (!loginValidation.test(String(value).toLowerCase())) {
-                return 'Invalid login';
+                return i18n.t('notification_invalid_login');
             } 
             return '';
         case 'password':
             if (!passwordValidation.test(String(value).toLowerCase())) {
-                return (!value) ? 'Password can`t be empty' : 'Invalid Password';
+                return (!value)
+                    ? i18n.t('notification_invalid_password_empty')
+                    : i18n.t('notification_invalid_password');
             }
             return ''
         default:
